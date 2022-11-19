8744ce98b1f64b7875edf716507da6b8f361c20b
diyorbekmajidov <majidovdiyorbek741@gmail.com>
2022-11-19 13:28:24 +0500

    serializers

---
+++ api/serializers.py
@@ -0,0 +1,3 @@
+from .models import Task
+from rest_framework import serializers
+
--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from rest_framework.request import Request
+
+@api_view(['GET'])
+def home(request:Request):
+    data=request.data
+    print(data)
+
+    return Response({'result':data})

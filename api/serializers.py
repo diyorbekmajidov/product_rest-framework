160910d928328a7adb268fa6bd5318908ece9d88
diyorbekmajidov <majidovdiyorbek741@gmail.com>
2022-11-26 09:41:01 +0500

    serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,3 +1,14 @@
 from .models import Task
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
+class TaskSerializer(serializers.Serializer):
+    task=serializers.CharField(max_length=255)
+    description=serializers.CharField(max_length=255)
+    status=serializers.BooleanField(default=False) 
+    created_at=serializers.DateTimeField(read_only=True)
+    updated_at=serializers.DateTimeField(read_only=True)
+    user=serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    
+    def create(self, validated_data):
+        return Task.objects.create(**validated_data)  
